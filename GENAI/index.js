d255f8f802f0320dad789bcdc56f506a2fb8bba0
Reshoo-18 <reshooranjan7@gmail.com>
2026-07-22 13:12:07 +0530

    LLM call done

--- GENAI/index.js
+++ GENAI/index.js
@@ -1,47 +1,65 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { GoogleGenAI } from '@google/genai'
-import OpenAI from "openai";
+import {ChatGroq} from "@langchain/groq"
+
 dotenv.config()
 const app=express()
 
 const PORT=3003
 app.use(express.json())
 
+// Without Langchain
+
+// const ai = new OpenAI({
+//     apiKey: process.env.GROQ_API_KEY,
+//     baseURL: "https://api.groq.com/openai/v1",
+// });
+
+// app.post('/ai',async(req,res)=>{
+//     const {contentt}=req.body
+//     const response = await ai.responses.create({
+//     model: "llama-3.3-70b-versatile",
+//     input: [
+//         {
+//             role: "system",
+//         content: `
+// You are an AI assistant named Jarvis.
+
+// Always introduce yourself at the beginning of every response by saying:
+// "Hey, I'm Jarvis."
+
+// After that, answer the user's question.`,
+
+//         },
+//       {
+//         role: "user",
+//         content:contentt
+//       },
+//     ]
+// });
+// res.status(200).json({"ai:-":response.output_text})
+// })
 
 
-const ai = new OpenAI({
-    apiKey: process.env.GROQ_API_KEY,
-    baseURL: "https://api.groq.com/openai/v1",
-});
+// With LAngchain
 
-app.post('/ai',async(req,res)=>{
-    const {contentt}=req.body
-    const response = await ai.responses.create({
-    model: "llama-3.3-70b-versatile",
-    input: [
-        {
-            role: "system",
-        content: `
-You are an AI assistant named Jarvis.
-
-Always introduce yourself at the beginning of every response by saying:
-"Hey, I'm Jarvis."
-
-After that, answer the user's question.`,
-
-        },
-      {
-        role: "user",
-        content:contentt
-      },
-    ]
-});
-res.status(200).json({"ai:-":response.output_text})
+const llm=new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temperature:2
 })
 
+app.post("/ai",async(req,res)=>{
+    const {input}=req.body
+    const response=await llm.invoke([{
+        role:"system",
+        content:"you are an ai assitant your name is jarvis"
+    },{
+        role:"human",
+        content:input
+    }])
 
-
+    res.status(200).json({"ai:":response.content})
+})
 
 app.get("/",(req,res)=>{
     res.send("hello i am server")
